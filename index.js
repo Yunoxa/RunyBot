3aaa1fb569bc89a6a6493372423e369a24dd86e2
Yunoxa <agentreborn3@gmail.com>
2024-12-17 15:03:36 +0000

    Set up project

---
+++ index.js
@@ -0,0 +1,27 @@
+import { Client } from "eris";
+import "dotenv/config";
+
+const client = new Client(`Bot ${process.env.TOKEN}`, {
+    intents: ["guilds", "guildMembers", "guildPresences", "guildMessages"]
+});
+
+client.on("ready", () => {
+    const guilds = JSON.parse(process.env.GUILDIDLIST);
+    guilds.forEach(guildID => {
+        client.createGuildCommand(guildID, {
+            name: "pop",
+            description: "Pops",
+        });
+    });
+
+    console.log("Ready!");
+});
+
+client.on("interactionCreate", (interaction) => {
+    console.log(interaction.data)
+    if(interaction.data.name === "pop") {
+        interaction.createMessage("***Pop*** ***Pop*** ***Pop***");
+    }
+});
+
+client.connect();
