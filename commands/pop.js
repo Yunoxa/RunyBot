fb31a5ecf1f745309e9b1056aeb63906fc8f7cf3
Yunoxa <agentreborn3@gmail.com>
2025-01-14 20:00:59 +0000

    Simplified popping

--- commands/pop.js
+++ commands/pop.js
@@ -4,15 +4,7 @@ const Constants = Eris.Constants;
 module.exports = {
     name: "pop",
     description: "pops",
-    options: [
-        {
-          "name": "target",
-          "description": "Target of popping.",
-          "type": Constants.ApplicationCommandOptionTypes.STRING,
-          "required": true,
-        }
-    ],
-    execute(interaction) {
-        interaction.createFollowup(`Runybot stares at ${interaction.data.options[0].value} and ***Pops***`);
+    async execute(interaction) {
+        interaction.createFollowup(`***pop pop pop***`);
     }
 };
